ac9fca44956c108b22ece6c31683dcbc1f43939a
wayha1 <vehaseng1122@gmail.com>
2025-09-18 13:57:28 +0700

    fix

--- src/main/java/com/practice/crud_api/entity/User.java
+++ src/main/java/com/practice/crud_api/entity/User.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -33,7 +34,7 @@ public class User implements UserDetails {
     // @Column(name = "last_name")
     private String lastName;
 
-    // @Column(name = "email", nullable = false, unique = true)
+    @Column(unique = true)
     private String email;
 
     private String password;
